9074f55de79cae0b1f7dd82efc7127f0781352c2
70gaurav <gk17265@gmail.com>
2023-02-14 09:30:16 +0530

    second commit

--- quiz.js
+++ quiz.js
@@ -5,6 +5,12 @@ let createuser = document.querySelector(".input")
 let adduser = document.querySelector(".add")
 let uname = document.querySelector(".input .name")
 let slide = document.querySelector(".wrapper")
+let slide2 = document.querySelector(".slide2")
+let user1 = document.querySelector(".user1")
+let anchors = document.querySelectorAll(".music a")
+let border = document.querySelector(".border")
+let options = document.querySelectorAll(".music input")
+let startquiz = document.querySelector(".music button")
 start.onclick = (e) =>{
     e.preventDefault()
     if(user.innerHTML.length===0){
@@ -12,6 +18,8 @@ start.onclick = (e) =>{
     }
     else{
         slide.style.display = "none"
+        slide2.style.display = "block"
+        user1.innerHTML = uname.value
     }
     
 }
@@ -25,4 +33,10 @@ adduser.onclick = (e) => {
     createuser.style.display = "none"
     user.innerHTML = uname.value
     alert("user created")
+}
+for(let i = 0; i<anchors.length;i++){
+    anchors[i].onclick = (e) =>{
+        e.preventDefault()
+        anchors[i].classList.add("border")
+    }
 }
